184eece3a8fffe4c06620c38afe14b13d4e49acc
Galax-Git <42323485+Galax-Git@users.noreply.github.com>
2018-08-12 15:22:05 -0500

    Add files via upload

---
+++ Radiology/Source/HediffDefOf.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace Radiology
+{
+    [DefOf]
+    public static class HediffDefOf
+    {
+        static HediffDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(HediffDefOf));
+        }
+
+        public static HediffDef RadiologyRadiation;
+        public static HediffMutationDef MutationFastLegs;
+        public static CancerDef RadiologyCancer;
+    }
+}
